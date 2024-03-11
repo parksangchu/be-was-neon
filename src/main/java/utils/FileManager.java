c4eff65c0f705c1d1b0aa1a0082cb98cc979d23f
sangchu <didi1484@gmail.com>
2024-03-11 19:37:00 +0900

    fix: FileManger 상수처리 및 getStaticFilePath 메서드 로직 변경
    - url 경로가 디렉토리일시 index.html 반환

--- src/main/java/utils/FileManager.java
+++ src/main/java/utils/FileManager.java
@@ -6,38 +6,43 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class FileManager {
-    public static final String HOME_PATH = "/";
-    public static final String DEFAULT_URL = "/index.html";
+    public static final String DEFAULT_FILE = "/index.html";
     public static final String STATIC_DEFAULT_DIRECTORY_PATH = "./src/main/resources/static";
+    public static final String REQUEST_HEADER_FILED_SEPARATOR = " ";
+    public static final String REQUEST_HEADER_LINE_SEPARATOR = "\n";
+    public static final String FILE_EXTENSION_MARKER = ".";
+    public static final String FILE_SEPARATOR = "/";
+    public static final String NO_FILE_EXTENSION = "";
 
     public static String getRequestHeader(InputStream in) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
         String line;
         StringBuilder requestHeaderBuilder = new StringBuilder();
         while (!(line = br.readLine()).isEmpty()) {
-            requestHeaderBuilder.append(line).append("\n");
+            requestHeaderBuilder.append(line).append(REQUEST_HEADER_LINE_SEPARATOR);
         }
         return requestHeaderBuilder.toString();
     }
 
     public static String getUrl(String requestHeader) {
-        return requestHeader.split("\n")[0]
-                .split(" ")[1];
+        return requestHeader.split(REQUEST_HEADER_LINE_SEPARATOR)[0]
+                .split(REQUEST_HEADER_FILED_SEPARATOR)[1];
     }
 
     public static String getStaticFilePath(String url) {
-        if (url.equals(HOME_PATH)) {
-            url = DEFAULT_URL;
+        String filePath = STATIC_DEFAULT_DIRECTORY_PATH + url;
+        if (url.contains(FILE_EXTENSION_MARKER)) {
+            return filePath;
         }
-        return STATIC_DEFAULT_DIRECTORY_PATH + url;
+        return filePath + DEFAULT_FILE;
     }
 
     public static String getFileExtension(String filePath) {
-        int lastIndexOfDot = filePath.lastIndexOf(".");
-        int lastIndexOfSlash = filePath.lastIndexOf("/");
+        int lastIndexOfDot = filePath.lastIndexOf(FILE_EXTENSION_MARKER);
+        int lastIndexOfSlash = filePath.lastIndexOf(FILE_SEPARATOR);
 
         if (lastIndexOfDot == -1 || lastIndexOfDot < lastIndexOfSlash) {
-            return ""; // 확장자가 없음을 나타냅니다.
+            return NO_FILE_EXTENSION; // 확장자가 없음을 나타냅니다.
         }
 
         return filePath.substring(lastIndexOfDot + 1);
