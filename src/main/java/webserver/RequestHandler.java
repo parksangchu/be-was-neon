1d55f64910b00d21521858535ab370880b6fe375
sangchu <didi1484@gmail.com>
2024-03-11 11:45:52 +0900

    feat: 리퀘스트 헤더 출력 기능 구현
    - 리퀘스트 헤드 읽어 debug 로그로 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,12 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,7 +25,16 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line;
+            StringBuilder requestHeaderBuilder = new StringBuilder();
+            while (!(line = br.readLine()).isEmpty()) {
+                requestHeaderBuilder.append(line).append("\n");
+            }
+            String requestHeader = requestHeaderBuilder.toString();
+            printRequestHeader(requestHeader);
+
+            DataOutputStream dos = new DataOutputStream(out); // 데이터를 담아서 반환
             byte[] body = "<h1>Hello World</h1>".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
@@ -33,7 +43,17 @@ public class RequestHandler implements Runnable {
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void printRequestHeader(String requestHeader) {
+        logger.debug("requestHeader= {}", requestHeader);
+
+        String requestLine = requestHeader.split("\n")[0];
+        logger.debug("requestLine= {}", requestLine);
+
+        String url = requestLine.split(" ")[1];
+        logger.debug("url={}", url);
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) { // 헤더 정보 입력
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
@@ -44,7 +64,7 @@ public class RequestHandler implements Runnable {
         }
     }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
+    private void responseBody(DataOutputStream dos, byte[] body) { // 바디 정보 입력
         try {
             dos.write(body, 0, body.length);
             dos.flush();
