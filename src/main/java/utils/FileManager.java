2922eb6d738cddb19589c127e38f9138839a5057
sangchu <didi1484@gmail.com>
2024-03-13 17:21:57 +0900

    fix: 필요없는 메서드 삭제
    - getFileBody 메서드 불필요하여 삭제함

--- src/main/java/utils/FileManager.java
+++ src/main/java/utils/FileManager.java
@@ -1,25 +1,9 @@
 package utils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
 public class FileManager {
-    public static final String DEFAULT_FILE = "/index.html";
-    public static final String STATIC_DEFAULT_DIRECTORY_PATH = "./src/main/resources/static";
-
     public static final String FILE_EXTENSION_MARKER = ".";
     public static final String FILE_SEPARATOR = "/";
     public static final String NO_FILE_EXTENSION = "";
-
-
-    public static String getStaticFilePath(String url) {
-        String filePath = STATIC_DEFAULT_DIRECTORY_PATH + url;
-        if (url.contains(FILE_EXTENSION_MARKER)) {
-            return filePath;
-        }
-        return filePath + DEFAULT_FILE;
-    }
 
     public static String getFileExtension(String filePath) {
         int lastIndexOfDot = filePath.lastIndexOf(FILE_EXTENSION_MARKER);
@@ -30,15 +14,5 @@ public class FileManager {
         }
 
         return filePath.substring(lastIndexOfDot + 1);
-    }
-
-    public static byte[] getFileBody(String filePath) throws IOException {
-        byte[] body;
-        File file = new File(filePath);
-        try (FileInputStream fis = new FileInputStream(file)) {
-            body = new byte[(int) file.length()];
-            fis.read(body);
-        }
-        return body;
     }
 }
