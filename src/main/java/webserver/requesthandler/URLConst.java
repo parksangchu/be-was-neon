05d446bdb406a414b7da4666ee19f6bf7c3d21c7
sangchu <didi1484@gmail.com>
2024-03-18 18:21:00 +0900

    feat: URLConst 추가
    URL 관리 용이성을 위해 URL 상수 추가하였음

---
+++ src/main/java/webserver/requesthandler/URLConst.java
@@ -0,0 +1,15 @@
+package webserver.requesthandler;
+
+public interface URLConst {
+    String HOME_URL = "/";
+    String LOGGED_IN_USER_HOME_URL = "/main";
+    String REGISTRATION_URL = "/registration";
+    String LOGIN_URL = "/login";
+    String LOGOUT_URL = "/logout";
+    String ARTICLE_URL = "/article";
+    String COMMENT_URL = "/comment";
+    String STATIC_DIRECTORY = "/static";
+    String INDEX_HTML_FILE = "/index.html";
+    String IMG_FILE_URL = "/img/.*";
+    String CSS_FILE_URL = "/css/.*";
+}
