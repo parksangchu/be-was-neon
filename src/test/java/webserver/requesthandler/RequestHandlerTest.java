d7d32053e410d948a5281ccfd3c61c2298f2f13b
sangchu <didi1484@gmail.com>
2024-03-18 22:50:57 +0900

    test: RequestHandler 테스트 추가
    - 정적 HTML 파일 설정 메서드 검증하였음

---
+++ src/test/java/webserver/requesthandler/RequestHandlerTest.java
@@ -0,0 +1,27 @@
+package webserver.requesthandler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.util.Objects;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import webserver.http.HttpResponse;
+
+class RequestHandlerTest {
+
+    @Test
+    @DisplayName("파라미터로 입력받은 경로의 index.html 파일을 response 의 바디로 설정할 수 있다.")
+    void setHTMLToBody() throws IOException {
+        RequestHandler requestHandler = new ArticleHandler();
+        HttpResponse response = new HttpResponse();
+        requestHandler.setHTMLToBody(response, "/article");
+        byte[] body = response.getBody();
+        assertThat(body).isEqualTo(getExpected());
+    }
+
+    private byte[] getExpected() throws IOException {
+        return Objects.requireNonNull(getClass().getResourceAsStream("/static/article/index.html"))
+                .readAllBytes();
+    }
+}
