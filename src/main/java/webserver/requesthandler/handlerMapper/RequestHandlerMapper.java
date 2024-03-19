7fead6374a29fdce9479f608e6604c49cf5a7f93
sangchu <didi1484@gmail.com>
2024-03-19 16:48:29 +0900

    feat: requestHandlerMapper 클래스 추가
    URL에 따른 Handler 매핑 기능을 분리하기 위해 해당 클래스 추가

---
+++ src/main/java/webserver/requesthandler/handlerMapper/RequestHandlerMapper.java
@@ -0,0 +1,40 @@
+package webserver.requesthandler.handlerMapper;
+
+import static webserver.requesthandler.URLConst.ARTICLE_URL;
+import static webserver.requesthandler.URLConst.COMMENT_URL;
+import static webserver.requesthandler.URLConst.HOME_URL;
+import static webserver.requesthandler.URLConst.LOGIN_URL;
+import static webserver.requesthandler.URLConst.LOGOUT_URL;
+import static webserver.requesthandler.URLConst.REGISTRATION_URL;
+import static webserver.requesthandler.URLConst.USER_LIST_URL;
+
+import java.util.HashMap;
+import java.util.Map;
+import webserver.http.HttpRequest;
+import webserver.requesthandler.handlerimpl.ArticleHandler;
+import webserver.requesthandler.handlerimpl.CommentHandler;
+import webserver.requesthandler.handlerimpl.HomeHandler;
+import webserver.requesthandler.handlerimpl.LoginHandler;
+import webserver.requesthandler.handlerimpl.LogoutHandler;
+import webserver.requesthandler.handlerimpl.RegistrationHandler;
+import webserver.requesthandler.handlerimpl.RequestHandler;
+import webserver.requesthandler.handlerimpl.StaticResourceHandler;
+import webserver.requesthandler.handlerimpl.UserListHandler;
+
+public class RequestHandlerMapper {
+    private static final Map<String, RequestHandler> store = new HashMap<>();
+
+    public RequestHandlerMapper() {
+        store.put(HOME_URL, new HomeHandler());
+        store.put(REGISTRATION_URL, new RegistrationHandler());
+        store.put(LOGIN_URL, new LoginHandler());
+        store.put(LOGOUT_URL, new LogoutHandler());
+        store.put(ARTICLE_URL, new ArticleHandler());
+        store.put(COMMENT_URL, new CommentHandler());
+        store.put(USER_LIST_URL, new UserListHandler());
+    }
+
+    public RequestHandler findRequestHandler(HttpRequest request) {
+        return store.getOrDefault(request.getPath(), new StaticResourceHandler());
+    }
+}
