37c9b241afbded0c5af6e52389b68019207d278d
sangchu <didi1484@gmail.com>
2024-03-19 14:18:48 +0900

    feat: 로그인시 홈화면에 유저 ID 출력하는 기능 추가

--- src/main/java/webserver/requesthandler/HomeHandler.java
+++ src/main/java/webserver/requesthandler/HomeHandler.java
@@ -1,12 +1,19 @@
 package webserver.requesthandler;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import model.User;
+import webserver.http.ContentType;
 import webserver.http.HttpRequest;
 import webserver.http.HttpResponse;
 import webserver.session.SessionManager;
 
 public class HomeHandler implements RequestHandler {
+
+    public static final Pattern LOGGED_IN_USER_NICKNAME_PATTERN = Pattern.compile(
+            "<p class=\"post__account__nickname\">.*?</p>");
 
     @Override
     public void handleGet(HttpRequest request, HttpResponse response) throws IOException {
@@ -15,11 +22,21 @@ public class HomeHandler implements RequestHandler {
             setHTMLToBody(response, URLConst.HOME_URL);
             return;
         }
-        setHTMLToBody(response, URLConst.LOGGED_IN_USER_HOME_URL);
+        setHTMLToBodyByUserId(response, user);
     }
 
     @Override
     public void handlePost(HttpRequest request, HttpResponse response) {
         response.setNotFound();
+    }
+
+    private void setHTMLToBodyByUserId(HttpResponse response, User user) throws IOException {
+        setHTMLToBody(response, URLConst.LOGGED_IN_USER_HOME_URL);
+        String content = new String(response.getBody(), StandardCharsets.UTF_8);
+
+        Matcher matcher = LOGGED_IN_USER_NICKNAME_PATTERN.matcher(content);
+        byte[] modifiedBody = matcher.replaceAll("<p class=\"post__account__nickname\">" + user.getUserId() + "</p>")
+                .getBytes(); // html의 account 부분을 아이디로 변경
+        response.setBody(modifiedBody, ContentType.HTML);
     }
 }
