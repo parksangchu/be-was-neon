9af3c0bc80b1dbd6f5b90e19d65af29ea97f4ca6
sangchu <didi1484@gmail.com>
2024-03-19 16:48:29 +0900

    feat: requestHandlerMapper 클래스 추가
    URL에 따른 Handler 매핑 기능을 분리하기 위해 해당 클래스 추가

--- src/main/java/webserver/requesthandler/MainRequestHandler.java
+++ src/main/java/webserver/requesthandler/MainRequestHandler.java
@@ -1,44 +1,27 @@
 package webserver.requesthandler;
 
-import static webserver.requesthandler.URLConst.ARTICLE_URL;
-import static webserver.requesthandler.URLConst.COMMENT_URL;
-import static webserver.requesthandler.URLConst.HOME_URL;
-import static webserver.requesthandler.URLConst.LOGIN_URL;
-import static webserver.requesthandler.URLConst.LOGOUT_URL;
-import static webserver.requesthandler.URLConst.REGISTRATION_URL;
-import static webserver.requesthandler.URLConst.USER_LIST_URL;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.http.HttpRequest;
 import webserver.http.HttpResponse;
 import webserver.requesthandler.authenticator.Authenticator;
 import webserver.requesthandler.authenticator.UnauthenticatedURLs;
-import webserver.requesthandler.handlerimpl.ArticleHandler;
-import webserver.requesthandler.handlerimpl.CommentHandler;
-import webserver.requesthandler.handlerimpl.HomeHandler;
-import webserver.requesthandler.handlerimpl.LoginHandler;
-import webserver.requesthandler.handlerimpl.LogoutHandler;
-import webserver.requesthandler.handlerimpl.RegistrationHandler;
+import webserver.requesthandler.handlerMapper.RequestHandlerMapper;
 import webserver.requesthandler.handlerimpl.RequestHandler;
-import webserver.requesthandler.handlerimpl.StaticResourceHandler;
-import webserver.requesthandler.handlerimpl.UserListHandler;
 
 public class MainRequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(MainRequestHandler.class);
 
     private final Socket connection;
-    private final Map<String, RequestHandler> requestHandlers;
+    private final RequestHandlerMapper requestHandlerMapper;
 
     public MainRequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
-        requestHandlers = initRequestHandlers();
+        requestHandlerMapper = new RequestHandlerMapper();
     }
 
     public void run() {
@@ -56,28 +39,12 @@ public class MainRequestHandler implements Runnable {
             if (!isAuthenticated) {
                 return;
             }
-            RequestHandler requestHandler = findRequestHandler(request);
+            RequestHandler requestHandler = requestHandlerMapper.findRequestHandler(request);
             handleMethod(request, response, requestHandler);
             response.send();
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
-    }
-
-    private Map<String, RequestHandler> initRequestHandlers() {
-        Map<String, RequestHandler> requestHandlers = new HashMap<>();
-        requestHandlers.put(HOME_URL, new HomeHandler());
-        requestHandlers.put(REGISTRATION_URL, new RegistrationHandler());
-        requestHandlers.put(LOGIN_URL, new LoginHandler());
-        requestHandlers.put(LOGOUT_URL, new LogoutHandler());
-        requestHandlers.put(ARTICLE_URL, new ArticleHandler());
-        requestHandlers.put(COMMENT_URL, new CommentHandler());
-        requestHandlers.put(USER_LIST_URL, new UserListHandler());
-        return requestHandlers;
-    }
-
-    private RequestHandler findRequestHandler(HttpRequest request) {
-        return requestHandlers.getOrDefault(request.getPath(), new StaticResourceHandler());
     }
 
     private void handleMethod(HttpRequest request, HttpResponse response, RequestHandler requestHandler)
